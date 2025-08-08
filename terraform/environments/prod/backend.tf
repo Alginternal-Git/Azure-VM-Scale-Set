ea312e0bdac7b62216a42ca457c345edb31fdeab
Alginternal-Git <notifications@algorims.com>
2025-08-08 14:05:04 +0530

    Add files via upload

---
+++ terraform/environments/prod/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "anv"      
+    storage_account_name = "anvv"      
+    container_name       = "anvvv"                 
+    key                  = "VMSS2.tfstate"  
+  }
+}
+
+
+
